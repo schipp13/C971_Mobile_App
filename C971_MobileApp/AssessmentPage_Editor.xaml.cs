12094f78a5d0339ca45aaab01ba0ac2e2e593d9c
Saige Chipp <42128866+schipp13@users.noreply.github.com>
2022-01-25 22:44:49 -0700

    Add files via upload

---
+++ C971_MobileApp/AssessmentPage_Editor.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace C971_MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AssessmentPage_Editor : ContentPage
+    {
+        public AssessmentPage_Editor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C971_MobileApp/CoursePage_Editor.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace C971_MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoursePage_Editor : ContentPage
+    {
+        public CoursePage_Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenAssessmentsPage_Editor(Object sender, EventArgs e)
+        {
+            App.Current.MainPage = new AssessmentPage_Editor();
+        }
+    }
+}
