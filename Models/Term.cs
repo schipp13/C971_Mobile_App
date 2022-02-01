64bfd2551bc5ee9b24db6bfc98df7b08f92ef456
Saige Chipp <42128866+schipp13@users.noreply.github.com>
2022-01-31 23:30:05 -0700

    Add files via upload

---
+++ Models/Term.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace c971_MobileApplication.Models
+{
+    public class Term
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Term_Id { get; set; }
+        public string Term_Name { get; set; }
+        public DateTime Term_Start { get; set; }
+        public DateTime Term_End { get; set; }
+        public ObservableCollection<Course> CourseList { get; set; }
+       
+    }
+}
---
+++ Views/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using c971_MobileApplication.Views;
+using System.Text;
+using Xamarin.Forms;
+
+namespace c971_MobileApplication
+{
+    public partial class AppShell : Shell
+    {
+
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(CoursePage), typeof(CoursePage));
+        }
+    }
+}
